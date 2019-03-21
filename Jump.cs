50d7baa8eca5cbc48d3086ef72a9d712024ee1bc
Unknown <unknown@example.com>
2019-03-22 00:34:36 +0800

    -New Script For Jump and Timer    Jump and Ed script put in the player hierachy -Timer script put it on its own hierachy (Text)

---
+++ Jump.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Jump : MonoBehaviour
+{
+
+    [SerializeField] Image jumpForceBar;
+    public static Rigidbody2D rb;
+    float holdTime;
+   
+     
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        jumpForceBar.fillAmount = 0f;
+    }
+    
+        void Update()
+        {       
+           if (Input.GetKey(KeyCode.Space))
+           {
+            StartCoroutine("StartCounting");
+            jumpForceBar.fillAmount = 0f;
+           }
+           
+           if (Input.GetKeyUp(KeyCode.Space))
+           {
+            StopCoroutine("StopCounting");
+
+            if (holdTime < 0.2f)
+                Ed.DoJump(20f);
+            else
+                Ed.DoJump(holdTime * 95f);
+
+            jumpForceBar.fillAmount = 0f;
+        }
+    }
+
+    IEnumerator StartCounting()
+    {
+        for (holdTime = 0f; holdTime <= 1f; holdTime += Time.deltaTime)
+        {
+            jumpForceBar.fillAmount = holdTime;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+        holdTime = 1f;
+        jumpForceBar.fillAmount = holdTime;
+    }
+}
---
+++ Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    private float time = 179;
+
+    void Start()
+    {
+        StartCoundownTimer();
+    }
+
+    void StartCoundownTimer()
+    {
+        if (timerText != null)
+        {
+            time = 179;
+            timerText.text = "3:00";
+            InvokeRepeating("UpdateTimer", 0.0f, 0.02f);
+        }
+    }
+
+    void UpdateTimer()
+    {
+        if (timerText != null)
+        {
+            time -= Time.deltaTime;
+            string minutes = Mathf.Floor(time / 60).ToString("00");
+            string seconds = (time % 60).ToString("00");
+            timerText.text = minutes + ":" + seconds;
+        }
+    }
+}
