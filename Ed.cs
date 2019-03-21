1a1e4434ac3fae6d1bd963dfb3f226730a24ca45
Unknown <unknown@example.com>
2019-03-22 00:05:51 +0800

    New Script for Ed

---
+++ Ed.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ed : MonoBehaviour {
+
+   
+    public static Rigidbody2D rb;
+    
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D> ();		
+	}
+
+    public static void DoJump(float jumpForce)
+    {
+        rb.AddForce(Vector2.up * jumpForce);
+    }
+}
