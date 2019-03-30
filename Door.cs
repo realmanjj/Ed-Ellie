117c3639efe490f4c9c65585e9be8f75fa385c9e
xiandi <xiandi31@gmail.com>
2019-03-30 16:24:03 +0800

    if Door belong to Ed, then apply ScoreCounter on Ellie scoreboard

---
+++ Door.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door2 : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col)
+    {
+        ScoreCounter.scoreAmount += 1;
+    }
+}
